d915426ce4271c4b1c970663246eef17a6601980
Artur <Artur@DEVIDE>
2022-08-02 17:28:54 +0300

    Фиксы багов

--- Controllers/CheckController.cs
+++ Controllers/CheckController.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using K_STEM_LAB.Models;
-using K_STEM_LAB.Adapter;
-using K_STEM_LAB.Models.Request;
-using RestSharp;
-using NLog;
-using Sentry;
-using System.Net.Mime;
+﻿using K_STEM_LAB.Adapter;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace K_STEM_LAB.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+  [Route("api/[controller]")]
+  [ApiController]
 
-    public class CheckController : ControllerBase
+  public class CheckController : ControllerBase
+  {
+
+    [HttpGet(Name = "GetCheck")]
+    public string GetCheck(string userName, string apiKey, string branch, int id_dogovor)
     {
-   
-        [HttpGet(Name = "GetCheck")]
-        public string GetCheck(string userName, string apiKey , string branch , int id_dogovor)
-        {
-            return CustomerBilling.Check(userName , apiKey , branch , id_dogovor);
-        }
+    
+        return CustomerBilling.Check(userName, apiKey, branch, id_dogovor);
+
+     
     }
+  }
 }
